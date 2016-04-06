819cbf6d8050b3da490178dd3fe572d576023955
Julio <jsavigny@gmail.com>
2016-04-06 21:39:18 +0700

    swap init and create window

--- HelloTriangle01.cpp
+++ HelloTriangle01.cpp
@@ -33,9 +33,9 @@ void display() {
  
 int main(int argc, char** argv) {
    glutInit(&argc, argv);                 // Initialize GLUT
-   glutCreateWindow("Hello Triangle 1"); // Create a window with the given title
    glutInitWindowSize(320, 320);   // Set the window's initial width & height
    glutInitWindowPosition(50, 50); // Position the window's initial top-left corner
+   glutCreateWindow("Hello Triangle 1"); // Create a window with the given title
    glutDisplayFunc(display); // Register display callback handler for window re-paint
    glutMainLoop();           // Enter the event-processing loop
    return 0;
